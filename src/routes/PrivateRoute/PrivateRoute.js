a3f7253986bddb584612c84aea0ea912004e3414
Md Hamimul Haque <mdhamimulhaque@gmail.com>
2022-11-25 18:42:36 +0600

    email login done

---
+++ src/routes/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
+
+const PrivateRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
+
+    const location = useLocation();
+
+    if (user) {
+        return children;
+    }
+    return <Navigate to="/login" state={{ from: location }} replace />
+};
+
+export default PrivateRoute;
