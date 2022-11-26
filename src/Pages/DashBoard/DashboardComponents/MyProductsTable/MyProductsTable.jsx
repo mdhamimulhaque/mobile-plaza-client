9a1e2b964f1397cbc71530012a14f1f3d2b1b1c3
Md Hamimul Haque <mdhamimulhaque@gmail.com>
2022-11-26 21:35:26 +0600

    handle delete product

--- src/Pages/DashBoard/DashboardComponents/MyProductsTable/MyProductsTable.jsx
+++ src/Pages/DashBoard/DashboardComponents/MyProductsTable/MyProductsTable.jsx
@@ -1,34 +1,74 @@
 
-import React, { useContext, useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react'
+import Swal from 'sweetalert2';
 import { AuthContext } from '../../../../context/AuthProvider'
 import Loading from '../../../Shared/components/Loading/Loading';
 
+
 const MyProductsTable = () => {
-    const { user, loading, setLoading } = useContext(AuthContext);
-    const [bookData, setBookData] = useState([])
+    const { user } = useContext(AuthContext);
+    // const [bookData, setBookData] = useState([]);
 
-    // ---> my products
-    useEffect(() => {
-        fetch(`http://localhost:5000/my-products?email=${user?.email}`)
-            .then(res => res.json())
-            .then(data => {
-                if (loading) {
-                    return <Loading />
-                }
-                setBookData(data)
-            })
-            .finally(setLoading(false))
-    }, [user?.email])
+    const { data: bookData = [user?.email], isLoading, refetch } = useQuery({
+        queryKey: ['my-products'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/my-products?email=${user?.email}`)
+            const data = await res.json()
+            return data
+        }
+    })
+
+    if (isLoading) {
+        return <Loading />
+    }
 
 
-    console.log(bookData)
+    // ---> handle Delete products
+    const handleDeleteProduct = (id) => {
+
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#4F46E5',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                // --->method
+                fetch(`http://localhost:5000/my-products/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            // ---> toast delete msg
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Product has been deleted.',
+                                'success'
+                            )
+                            refetch()
+                        }
+                    })
+                    .catch(err => console.log(err))
+            }
+        })
+
+    }
 
     return (
         <div className="mx-auto pb-8 w-full max-w-7xl overflow-x-auto px-6">
             <table className="px-4 min-w-full rounded-md border border-gray-200 overflow-hidden">
 
-                {/* :TABLE HEAD */}
-                <thead className="min-w-full bg-gray-100 text-left text-gray-700 text-center">
+                <thead className="min-w-full bg-gray-100  text-gray-700 text-center">
                     <tr>
                         <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">User</th>
                         <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">Product Name</th>
@@ -40,8 +80,6 @@ const MyProductsTable = () => {
                     </tr>
                 </thead>
 
-
-                {/* :TABLE BODY */}
                 <tbody className="text-center">
                     {bookData?.map((booking, index) => (
                         <tr key={booking?._id} className={`${index % 2 === 0 ? "bg-gray-50" : "bg-gray-100"} whitespace-nowrap`}>
@@ -50,13 +88,13 @@ const MyProductsTable = () => {
                             <td className="py-3 px-4 text-base text-gray-500 font-medium">{booking?.price}</td>
                             <td className="py-3 px-4 text-base text-gray-500 font-medium">{booking?.email}</td>
                             <td className="py-3 px-4 text-base text-gray-500 font-medium text-center">
-                                <span className="cursor-pointer inline-flex justify-center items-center py-0.5 px-2.5 border-none rounded-full bg-green-100 text-xs text-green-800 font-medium">available</span>
+                                <span className="cursor-pointer inline-flex justify-center items-center py-0.5 px-2.5 border-none rounded-full bg-green-100 text-xs text-indigo-800 font-medium">available</span>
                             </td>
                             <td className="py-3 px-4 text-base text-gray-500 font-medium">
                                 {booking?.date}
                             </td>
                             <td className="py-3 px-4 flex justify-center items-center space-x-6 text-base text-gray-700 font-medium">
-                                <button type="button" className="text-sm text-red-500 font-semibold hover:text-red-600">Delete</button>
+                                <button onClick={() => handleDeleteProduct(booking?._id)} type="button" className="text-sm text-red-500 font-semibold hover:text-red-600">Delete</button>
                             </td>
                         </tr>
                     ))
