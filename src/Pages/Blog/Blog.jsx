4d146f8e74535199616fc28283ad459b0ae501e0
Md Hamimul Haque <mdhamimulhaque@gmail.com>
2022-11-28 01:39:34 +0600

    blog route

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,27 +1,37 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from '../Shared/components/Loading/Loading';
 
 
 
 const Blog = () => {
+    const { data: blogsItems = [], isLoading, refetch } = useQuery({
+        queryKey: ['blogs'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/blogs`);
+            const data = await res.json();
+            return data
+        }
+    });
 
-    // --->time
-    const today = new Date();
-    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    console.log(time)
-
-    let day = today.getDate();
-    let month = today.getMonth() + 1;
-    let year = today.getFullYear();
-
-    // ---> date
-    let date = `${day}-${month}-${year}`;
-    console.log(date);
-
+    if (isLoading) {
+        return <Loading />
+    }
 
     return (
-        <div>
-            blogs
+
+
+        <div className="flex flex-wrap gap-2 justify-center my-20 px-6">
+            {
+                blogsItems.map(item =>
+                    <div key={item.id} className="p-6 md:1/2 xl:w-1/4 shadow-lg">
+                        <h1 className="mb-3 inline-block title-font text-xl font-extrabold text-gray-800 tracking-wide cursor-pointer">{item.qn}</h1>
+                        <p className="line-clamp-6 mb-3 overflow-hidden leading-relaxed text-gray-500 cursor-pointer">{item.ans}</p>
+                    </div>
+                )
+            }
         </div>
+
     );
 };
 
